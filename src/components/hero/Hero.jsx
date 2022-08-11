2a23c58efd9dd9749735e87cd0c6472cc1e1d99c
martincavil <martin.cavil98@gmail.com>
2022-08-11 17:52:14 +0200

    add smooth scroll

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,14 +5,22 @@ export default function Hero() {
   return (
     <div id='hero' className='hero container'>
       <div className="left">
-        <h2>Avec Passion, qu'est-ce que c'est ?</h2>
-        <h1>GROS TITRE</h1>
+        <h1>Avec Passion, qu'est-ce que c'est ?</h1>
+        <p>
+          Salut, moi, c'est Nico, j'ai 23 ans, et je vais parcourir plusieurs pays pour récolter des fonds pour la recherche sur l'AVC.</p>
+          Un brin fan de vélo et désireux de traverser le plus grand nombre de frontières possible, ce trajet se fera en pédalant.
+        <p>
+          Le but est simple : 1€ donné pour la recherche sur l’AVC = 1km parcouru.
+          Plus je verrais de ville, plus je pourrais quêter en direct avec les autochtones pour récolter encore plus de sous.
+        </p>
         <div className='participants'></div>
         <a href="#" className='button1'>Je participe !</a>
       </div>
       <div className="right">
-          <div className="first-picture"></div>
-          <div className="second-picture"></div>
+          <div className="first-picture">
+            <img src="/assets/images/IMG_3766.jpg" alt="Papa en vacances" className='first-picture'/>
+          </div>
+          <img src='/assets/images/IMG_3767.jpg' alt='' className="second-picture" />
       </div>
     </div>
   )
--- src/components/journey/Journey.jsx
+++ src/components/journey/Journey.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import './journey.scss'
 export default function Journey() {
   return (
-    <div id='journey'>Journey</div>
+    <div id='journey' className='journey container'>
+      <div>
+        <h2>Départ de Lyon</h2>
+        <h2>Le 16 septembre 2022</h2>
+      </div>
+        <p>
+          Toutes les informations sur mon trajet d'ici peu 🙂
+        </p>
+    </div>
   )
 }
